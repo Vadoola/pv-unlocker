decb3176d365b692d31d94daacc2952a6d650342
Carl <carl@vadoola.com>
2023-08-15 14:53:02 +1200

    Have unlock callbacks working and unlocking the files.
    Still need to get the model updated with the new status though.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@
 //#![deny(clippy::cargo)]
 //#![deny(missing_docs)]
 
+use std::borrow::BorrowMut;
 use std::path::PathBuf;
 use clap::Parser;
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 use ab_versions::{get_version, is_protected, strip_protection};
-use slint::{Color, ModelRc, VecModel};
+use slint::{ModelRc, VecModel};
 use std::rc::Rc;
+use std::collections::HashMap;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -24,24 +26,42 @@ fn main() -> Result<(), slint::PlatformError> {
     let args = Args::parse_from(wild::args());
 
     let ui = AppWindow::new()?;
+    let files: HashMap<String, PathBuf> = args.files.iter().map(|pb| {
+        let name = pb.file_name().map(|name| name.to_string_lossy()).unwrap().to_string();
+        (name, pb.to_owned())
+    }).collect();
 
-    if !args.files.is_empty() {
-        let info: Vec<file_info> = args.files.par_iter().map(|f| {
+    let mut file_model = Rc::new(VecModel::default());
+
+    //if files were passed on the command line process them and add them to the UI model
+    if !files.is_empty() {
+        let info: Vec<file_info> = files.par_iter().map(|(name, file)| {
             file_info {
-                locked: is_protected(f).unwrap(),
-                file_name: format!("{:?}", f.file_name().unwrap()).into(),
-                file_ver: get_version(f).unwrap().to_string().into(),
+                locked: is_protected(&file).unwrap(),
+                file_name: name.into(),
+                file_ver: get_version(&file).unwrap().to_string().into(),
             }
         }).collect();
-        let info = ModelRc::from(Rc::new(VecModel::from(info)));
-        ui.set_files(info);
+        file_model.borrow_mut().extend(info.into_iter());
     }
 
-    /*let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
-    });*/
+    ui.set_files(ModelRc::from(file_model));
+
+    let unlock_handle = ui.as_weak();
+    ui.on_unlock(move |file| {
+        let unlock_handle = unlock_handle.upgrade().unwrap();
+
+        if let Some(path) = files.get(&file.to_string()) {
+            strip_protection(path).unwrap();
+            /*let mdl = unlock_handle.get_files();
+            let name = file.clone();
+            mdl.map(move |mut fi| {
+                if fi.file_name == name {
+                    fi.locked = is_protected(path).unwrap();
+                }
+            });*/
+        }//else display some sort of toast message with the error?
+    });
 
     ui.run()
 }
